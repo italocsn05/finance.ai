0639f30e6bf7ea2f257bd1db948750f16d487a8d
Italo Cabral <italocsn05@gmail.com>
2025-01-29 21:33:01 -0300

    Add logo SVG, enhance login page layout, and update global styles for full height

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,39 @@
 import { Button } from "@/components/ui/button";
+import { LogInIcon } from "lucide-react";
+import Image from "next/image";
 
 const HomePage = () => {
-  return <div>
-    <Button>RAPARIGA</Button>
-  </div>;
+  return (
+    <div className="grid h-full grid-cols-2">
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          width={173}
+          height={39}
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
+        <p className="mb-8 text-muted-foreground">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
+        <Button variant={"outline"}>
+          <LogInIcon className="mr-2" />
+          Fazer login ou criar conta
+        </Button>
+      </div>
+      <div className="relative h-full w-full">
+        <Image
+          src="/side-login-image.png"
+          alt="side-image"
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
