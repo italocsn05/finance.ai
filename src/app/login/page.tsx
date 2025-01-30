1dc1a34a421dc88c74f680793e2bfde36ce5d78a
Italo Cabral <italocsn05@gmail.com>
2025-01-29 21:10:54 -0300

    Implement utility functions, update login page with button component, enhance layout for dark mode, and configure Tailwind CSS

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+  return <div>
+    <Button>RAPARIGA</Button>
+  </div>;
 };
 
 export default HomePage;
