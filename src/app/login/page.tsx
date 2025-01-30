76cb86e8ab3b35a71274290a4b0171a6a14f0010
Italo Cabral <italocsn05@gmail.com>
2025-01-29 21:49:22 -0300

    Add Clerk authentication integration, update login and transactions pages, and configure middleware

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { LogInIcon } from "lucide-react";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/transactions");
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
@@ -19,10 +27,12 @@ const HomePage = () => {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant={"outline"}>
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant={"outline"}>
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
       <div className="relative h-full w-full">
         <Image
--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,9 +1,17 @@
-const TransactionsPage = () => {
-    return ( 
-        <div>
-            <h1>Transactions</h1>
-        </div>
-     );
-}
- 
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const TransactionsPage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return (
+    <div>
+      <UserButton showName></UserButton>
+    </div>
+  );
+};
+
 export default TransactionsPage;
