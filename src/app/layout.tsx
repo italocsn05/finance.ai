8741bce790dd26b0e7e464427d8d0a542f0aa7ac
Italo Cabral <italocsn05@gmail.com>
2025-02-03 19:13:42 -0300

    feat: update layout styles and replace font with Mulish, add DANFE.pdf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+import { Mulish } from "next/font/google";
+const mulish = Mulish({
+  subsets: ["latin-ext"],
 });
 
 export const metadata: Metadata = {
@@ -27,9 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}
-        >
+        <body className={`${mulish.className} dark antialiased`}>
           {children}
         </body>
       </html>
