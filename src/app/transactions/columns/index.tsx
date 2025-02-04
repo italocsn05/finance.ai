506584b9e0f79e7a0ac48ba5a6ef473fe5e17610
Italo Cabral <italocsn05@gmail.com>
2025-02-04 17:25:53 -0300

    feat: implement transaction data table with type badges and add Prisma client integration

---
+++ src/app/transactions/columns/index.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Transaction } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import TransactionTypeBadge from "../components/type-badge";
+
+export const transactionColumns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "type",
+    header: "Tipo",
+    cell: ({ row: { original: transaction } }) => {
+      return <TransactionTypeBadge transaction={transaction} />;
+    },
+  },
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Método de Pagamento",
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+  },
+  {
+    accessorKey: "actions",
+    header: "Ações",
+  },
+];
--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,15 +1,28 @@
-import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { ArrowDownUpIcon } from "lucide-react";
 import { redirect } from "next/navigation";
+import { transactionColumns } from "./columns";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
+  const transactions = await db.transaction.findMany({});
+
   if (!userId) {
     redirect("/login");
   }
   return (
-    <div>
-      <UserButton showName></UserButton>
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar Transação
+          <ArrowDownUpIcon></ArrowDownUpIcon>
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions}></DataTable>
     </div>
   );
 };
