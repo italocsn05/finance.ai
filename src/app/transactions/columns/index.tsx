bacfe298d8ecea25c62cb45de3fab0018c4693aa
Italo Cabral <italocsn05@gmail.com>
2025-02-04 17:50:09 -0300

    feat: enhance transaction table with category and payment method labels, add action buttons

--- src/app/transactions/columns/index.tsx
+++ src/app/transactions/columns/index.tsx
@@ -1,8 +1,32 @@
 "use client";
 
-import { Transaction } from "@prisma/client";
+import { Transaction, TransactionCategory } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import TransactionTypeBadge from "../components/type-badge";
+import { Button } from "@/components/ui/button";
+import { PenIcon, TrashIcon } from "lucide-react";
+
+export const TRANSACTION_CATEGORY_LABELS = {
+  [TransactionCategory.HOUSING]: "Moradia",
+  [TransactionCategory.FOOD]: "Alimentação",
+  [TransactionCategory.EDUCATION]: "Educação",
+  [TransactionCategory.ENTERTAINMENT]: "Entretenimento",
+  [TransactionCategory.SALARY]: "Salário",
+  [TransactionCategory.HEALTH]: "Saúde",
+  [TransactionCategory.TRANSPORTATION]: "Transporte",
+  [TransactionCategory.OTHER]: "Outros",
+  [TransactionCategory.UTILITY]: "Utilidades",
+};
+
+export const TRANSACTION_PAYMENT_METHOD_LABELS = {
+  CREDIT_CARD: "Cartão de Crédito",
+  DEBIT_CARD: "Cartão de Débito",
+  BANK_TRANSFER: "Transferência Bancária",
+  BANK_SLIP: "Boleto Bancário",
+  CASH: "Dinheiro",
+  PIX: "PIX",
+  OTHER: "Outros",
+};
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -19,21 +43,60 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
+    cell: ({ row: { original: transaction } }) => {
+      return TRANSACTION_CATEGORY_LABELS[transaction.category];
+    },
   },
   {
     accessorKey: "paymentMethod",
     header: "Método de Pagamento",
+    cell: ({ row: { original: transaction } }) => {
+      return TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod];
+    },
   },
   {
     accessorKey: "date",
     header: "Data",
+    cell: ({ row: { original: transaction } }) => {
+      return new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      });
+    },
   },
   {
     accessorKey: "amount",
     header: "Valor",
+    cell: ({ row: { original: transaction } }) => {
+      return new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount));
+    },
   },
   {
     accessorKey: "actions",
     header: "Ações",
+    cell: () => {
+      return (
+        <div className="space-x-1">
+          <Button
+            variant={"ghost"}
+            size="icon"
+            className="text-muted-foreground"
+          >
+            <TrashIcon></TrashIcon>
+          </Button>
+          <Button
+            variant={"ghost"}
+            size="icon"
+            className="text-muted-foreground"
+          >
+            <PenIcon></PenIcon>
+          </Button>
+        </div>
+      );
+    },
   },
 ];
